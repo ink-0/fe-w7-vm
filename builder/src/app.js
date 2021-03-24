0be861af82eadc7cd0365709242271497d9f9fe2
ink-0 <71919983+ink-0@users.noreply.github.com>
2021-03-25 02:14:26 +0900

    [REFACTOR] fix conflict

--- builder/src/app.js
+++ builder/src/app.js
@@ -1,7 +1,4 @@
 import { renderProduct } from "./productBox";
-<<<<<<< HEAD
-renderProduct();
-=======
 import { WalletView } from "./js/view/WalletView";
 import { WalletModel} from "./js/model/WalletModel";
 
@@ -12,4 +9,3 @@ const view = new WalletView(model);
 view.update();
 
 model.addObserver(view);
->>>>>>> 3fc5eb6810eaffd00ce0605410868f6c19214066
